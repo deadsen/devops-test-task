4b2ce35c37ac70c443995ddc5e20badf129e09fb
deadsen <56022407+deadsen@users.noreply.github.com>
2021-09-06 12:28:09 +0300

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker service update --force --image 224013136264.dkr.ecr.eu-central-1.amazonaws.com/deadsen/devops-test-task:latest chatbots-app_chatbots-app
+imagetag=$(aws ssm get-parameter --name /CodeBuild/imagetag --with-decryption | jq -r .Parameter.Value)
+docker service update --force --image 224013136264.dkr.ecr.eu-central-1.amazonaws.com/deadsen/devops-test-task:$imagetag chatbots-app_chatbots-app

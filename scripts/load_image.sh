f440c5fea7b24abbcd3ce4a36fa6ef4737c687fb
deadsen <56022407+deadsen@users.noreply.github.com>
2021-09-06 12:27:16 +0300

    added imagetag with parameter

--- scripts/load_image.sh
+++ scripts/load_image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+imagetag=$(aws ssm get-parameter --name /CodeBuild/imagetag --with-decryption | jq -r .Parameter.Value)
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 224013136264.dkr.ecr.eu-central-1.amazonaws.com
-docker pull 224013136264.dkr.ecr.eu-central-1.amazonaws.com/deadsen/devops-test-task:latest
+docker pull 224013136264.dkr.ecr.eu-central-1.amazonaws.com/deadsen/devops-test-task:$imagetag
